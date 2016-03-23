a7d069a852633a41d15fee35993188bfe065a66d
YoungLeeNENU <youngleemails@gmail.com>
2016-03-23 23:53:43 +0800

    block scope example

--- you-dont-know-js-es6-and-beyond/syntax/es5/block-scoped-declarations.js
+++ you-dont-know-js-es6-and-beyond/syntax/es5/block-scoped-declarations.js
@@ -1,9 +1,16 @@
+/**
+ * @fileOverview for block scope
+ * @name block-scoped-declarations.js<you-dont-know-js-es6-and-beyond/syntax/es6>
+ * @author Young Lee <youngleemails@gmail.com>
+ * @license MIT
+ */
 'use strict';
 
-{
-	console.log(a);
-	console.log(b);
-
-	var a = 1;
-	var b = 2;
+for (var _a = 0, b = '42', c = {
+    test: true
+}, blockScope = 0; blockScope < 1; blockScope++) {
+    // logic in this block
+    console.log(_a, b, c);
 }
+
+console.log(a);
--- you-dont-know-js-es6-and-beyond/syntax/es6/block-scoped-declarations.js
+++ you-dont-know-js-es6-and-beyond/syntax/es6/block-scoped-declarations.js
@@ -1,6 +1,19 @@
+/**
+ * @fileOverview for block scope
+ * @name block-scoped-declarations.js<you-dont-know-js-es6-and-beyond/syntax/es6>
+ * @author Young Lee <youngleemails@gmail.com>
+ * @license MIT
+ */
 'use strict';
-{
-	let b = 3;
 
-	console.log(b);
+for (let a = 0,
+         b = '42',
+         c = {
+             test: true
+         },
+         blockScope = 0; blockScope < 1; blockScope++) {
+             // logic in this block
+             console.log(a, b, c);
 }
+
+console.log(a);    // Referrence Error
